367be6f1a1271b005858d1572dc296b51f690db9
peppelin <peppelin@gmail.com>
2024-03-05 18:33:18 +0100

    aded switch case for routings

--- main.go
+++ main.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// TODO: set handler for page not found
+	}
+}
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
@@ -17,8 +27,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Cobtact Page</h1><p>To get in touch, email me at <a href=\"mailto:peppelin@gmail.com\">peppelin@gmail.com</a>")
 }
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Starting server on :3000")
 	http.ListenAndServe(":3000", nil)
 }
