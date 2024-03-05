8631c2a54fe2681819a4d768f8e08a7dea8cd717
peppelin <peppelin@gmail.com>
2024-03-05 18:06:53 +0100

    Adding content-type headers

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 func main() {
