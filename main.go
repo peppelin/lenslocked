c87a061f1d80614c8115e031dd8412c53d108d4a
peppelin <peppelin@gmail.com>
2024-03-05 18:22:47 +0100

    Added contact page. updated home handler

--- main.go
+++ main.go
@@ -5,13 +5,20 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	//w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Cobtact Page</h1><p>To get in touch, email me at <a href=\"mailto:peppelin@gmail.com\">peppelin@gmail.com</a>")
+}
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting server on :3000")
 	http.ListenAndServe(":3000", nil)
 }
