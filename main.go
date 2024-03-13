e0e857d56e45c5f1abc783e09ef28293e6a9a6e5
peppelin <peppelin@gmail.com>
2024-03-13 12:22:45 +0100

    adding controllers and refcatoring main

--- main.go
+++ main.go
@@ -4,92 +4,35 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"html/template"
-	"log"
+	"github.com/peppelin/lenslocked/controllers"
+	"github.com/peppelin/lenslocked/views"
 	"net/http"
 	"path/filepath"
 )
 
-/*type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}*/
-
-//type HandlerFunc func(w http.ResponseWriter, r *http.Request)
-
-/*
-	func (f HandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-		f(w, r)
-	}
-
-	func pathHandler(w http.ResponseWriter, r *http.Request) {
-		switch r.URL.Path {
-		case "/":
-			homeHandler(w, r)
-		case "/contact":
-			contactHandler(w, r)
-		default:
-			http.Error(w, "Page not found", http.StatusNotFound)
-		}
-	}
-*/
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "faq.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		log.Printf("parsing the template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
-		return
-	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing the template: %v", err)
-		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
-		return
-	}
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Set("Content-Type", "text/plain")
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
-}
-
-func galleryHandler(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Set("Content-Type", "text/plain")
-	id := chi.URLParam(r, "id")
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Write([]byte(fmt.Sprintf("ID: %s\n", id)))
-}
 func main() {
 	r := chi.NewRouter()
+
 	r.Use(middleware.Logger)
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
-	r.Get("/gallery/{id}", galleryHandler)
+
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/faq", controllers.StaticHandler(tpl))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page NOT Found", http.StatusNotFound)
 	})
