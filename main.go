866ba5b261741961b280a03052f82cd3ea527ea4
peppelin <peppelin@gmail.com>
2024-03-18 11:45:11 +0100

    FAQ templates and refactor

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page NOT Found", http.StatusNotFound)
